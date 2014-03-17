29844dc336de899774724299c9bea05cde430a88
David Maack <david.maack@arcor.de>
2014-03-17 18:02:39 +0100

    Added support for insertTags for the ContaoMap center

--- ContaoMap.php
+++ ContaoMap.php
@@ -9,7 +9,7 @@
  * @filesource
  */
 
-abstract class ContaoMap extends System
+abstract class ContaoMap extends \Controller
 {
 	protected $layerIds = NULL;
 	protected $arrLayer = array();
@@ -89,6 +89,8 @@ abstract class ContaoMap extends System
 		{
 			case 'center':
 				if(is_string($value))
+					$value=$this->replaceInsertTags($value);
+				if(is_string($value))
 					$value=explode(',', $value);
 				if(is_string($value))
 					$value=explode(' ', $value);
@@ -111,7 +113,7 @@ abstract class ContaoMap extends System
 				$this->viewPort[1] = $value;
 			break;
 			default:
-				throw new Exception('Can not set property '.$key);
+				parent::__set($key, $value);
 		}
 	}
 
@@ -141,7 +143,7 @@ abstract class ContaoMap extends System
 			case 'layer':
 				return $this->arrLayer;
 			default:
-				throw new Exception('Unknown property '.$key);
+				return parent::__get($key);
 			break;
 		}
 	}
